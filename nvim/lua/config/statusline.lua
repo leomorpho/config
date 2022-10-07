8009bdb5b2578dec7bfc80771f62713974296da1
leo audibert <laudibert@zenefits.com>
2022-10-07 11:09:11 -0700

    Add git hook for python

--- nvim/lua/config/statusline.lua
+++ nvim/lua/config/statusline.lua
@@ -163,13 +163,14 @@ require("lualine").setup {
   extensions = { "quickfix", "fugitive", "nvim-tree" },
 }
 
-function _G.symbol_line()
-  local curwin = vim.g.statusline_winid or 0
-  local curbuf = vim.api.nvim_win_get_buf(curwin)
-  local ok, line = pcall(vim.api.nvim_buf_get_var, curbuf, 'coc_symbol_line')
-  return ok and line or ''
-end
-
-vim.o.tabline = '%!v:lua.symbol_line()'
-vim.o.statusline = '%!v:lua.symbol_line()'
-vim.o.winbar = '%!v:lua.symbol_line()'
+-- SHOW PATH OF CURRENTLY EDITED FILE
+-- function _G.symbol_line()
+--   local curwin = vim.g.statusline_winid or 0
+--   local curbuf = vim.api.nvim_win_get_buf(curwin)
+--   local ok, line = pcall(vim.api.nvim_buf_get_var, curbuf, 'coc_symbol_line')
+--   return ok and line or ''
+-- end
+-- 
+-- vim.o.tabline = '%!v:lua.symbol_line()'
+-- vim.o.statusline = '%!v:lua.symbol_line()'
+-- vim.o.winbar = '%!v:lua.symbol_line()'
